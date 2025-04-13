f216cfbcde0be7cc4f5d35f4fcd3f763367736f0
Matsumoto4 <ruicube13dk@gmail.com>
2025-04-13 17:56:04 +0900

    初回のコミットメッセージ

---
+++ src/enshu1_pkg/Main.java
@@ -0,0 +1,30 @@
+package enshu1_pkg;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+//		//演習１,2
+//		System.out.print("こんにちは。");
+//		System.out.println("おはようございます。");
+		//演習3
+		String s = "こんにちは";
+		System.out.println(s);
+		//演習４
+		System.out.println("「 \"」はダブルクオーテーションです。");
+		//問題４
+		int a = 5;
+		System.out.println("aは　"+ a + " です");
+		//問題5
+		int a1 = 5;
+		double d = 3.14;
+		char c = 'A';
+		String st ="Hello";
+		System.out.println(a1);
+		System.out.println(d);
+		System.out.println(c);
+		System.out.println(st);
+	}
+	
+
+}
